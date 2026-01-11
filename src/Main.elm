481a95bcea90a42819e4bdc7080cfbcde9d96ba6
Javier Escalada <kerrigan29a@gmail.com>
2026-01-11 15:46:50 +0000

    Add URL-based state sharing for game verification
    - Change from Browser.element to Browser.application
    - Implement state encoding/decoding using JSON + Base64
    - Store icon indices instead of full paths in journal
    - Add Share button to generate shareable URLs
    - URL state takes precedence over localStorage
    - Update version to 0.5.0
    - Add required dependencies: elm/url, elm/bytes, truqu/elm-base64

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,9 @@ port module Main exposing (..)
 --   https://guide.elm-lang.org/effects/random.html
 
 import Array exposing (..)
+import Base64
 import Browser
+import Browser.Navigation as Nav
 import Dict
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -17,11 +19,14 @@ import Random
 import Svg exposing (image, svg)
 import Svg.Attributes as SvgAttr
 import Tables exposing (..)
+import Url
+import Url.Parser as Parser exposing (Parser)
+import Url.Parser.Query as Query
 
 
 version : String
 version =
-    "0.4.3"
+    "0.5.0"
 
 
 
@@ -29,11 +34,13 @@ version =
 
 
 main =
-    Browser.element
+    Browser.application
         { init = init
         , update = updateWithStorage
         , subscriptions = \_ -> Sub.none
         , view = view
+        , onUrlRequest = \_ -> NoOp
+        , onUrlChange = \_ -> NoOp
         }
 
 
@@ -70,16 +77,14 @@ encode model =
         ]
 
 
-decoder : D.Decoder Model
-decoder =
-    D.map7 Model
+decoder : Nav.Key -> D.Decoder Model
+decoder key =
+    D.map6 (\ds da de t ia j -> Model ds da de t ia [] j key)
         (D.field "dice_sides" D.int)
         (D.field "dice_amount" D.int)
         (D.field "dice_explode" D.bool)
         (D.field "table" D.string)
         (D.field "icon_amount" D.int)
-        (D.succeed [])
-        -- Le asigna una lista vacía a icons
         (D.field "journal" (D.list entryDecoder))
 
 
@@ -113,6 +118,7 @@ type alias Model =
     , icon_amount : Int
     , icons : List String
     , journal : List Entry
+    , key : Nav.Key
     }
 
 
@@ -121,13 +127,22 @@ type Entry
     | Img String
 
 
-init : E.Value -> ( Model, Cmd Msg )
-init flags =
-    ( case D.decodeValue decoder flags of
-        Ok model ->
-            model
+init : E.Value -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    let
+        -- Try URL first, then localStorage
+        modelFromUrl =
+            parseStateFromUrl url key
 
-        Err _ ->
+        modelFromStorage =
+            case D.decodeValue (decoder key) flags of
+                Ok model ->
+                    Just model
+
+                Err _ ->
+                    Nothing
+
+        defaultModel =
             { dice_sides = 6
             , dice_amount = 1
             , dice_explode = False
@@ -135,7 +150,23 @@ init flags =
             , icon_amount = 3
             , icons = []
             , journal = []
+            , key = key
             }
+
+        initialModel =
+            case modelFromUrl of
+                Just urlModel ->
+                    urlModel
+
+                Nothing ->
+                    case modelFromStorage of
+                        Just storageModel ->
+                            storageModel
+
+                        Nothing ->
+                            defaultModel
+    in
+    ( initialModel
     , Http.get
         { url = "./icons/icons.json"
         , expect = Http.expectJson GotIcons iconsDecoder
@@ -185,6 +216,82 @@ iconsDecoder =
 
 
 
+-- URL STATE ENCODING/DECODING
+
+
+parseStateFromUrl : Url.Url -> Nav.Key -> Maybe Model
+parseStateFromUrl url key =
+    url.query
+        |> Maybe.andThen
+            (\query ->
+                String.split "=" query
+                    |> List.Extra.getAt 1
+                    |> Maybe.andThen (decodeStateFromString key)
+            )
+
+
+decodeStateFromString : Nav.Key -> String -> Maybe Model
+decodeStateFromString key encoded =
+    encoded
+        |> Url.percentDecode
+        |> Maybe.andThen
+            (\decoded ->
+                Base64.decode decoded
+                    |> Result.toMaybe
+            )
+        |> Maybe.andThen (D.decodeString (urlStateDecoder key) >> Result.toMaybe)
+
+
+encodeStateToString : Model -> String
+encodeStateToString model =
+    encodeUrlState model
+        |> E.encode 0
+        |> Base64.encode
+        |> Url.percentEncode
+
+
+urlStateDecoder : Nav.Key -> D.Decoder Model
+urlStateDecoder key =
+    D.map6 (\ds da de t ia j -> Model ds da de t ia [] j key)
+        (D.field "dice_sides" D.int)
+        (D.field "dice_amount" D.int)
+        (D.field "dice_explode" D.bool)
+        (D.field "table" D.string)
+        (D.field "icon_amount" D.int)
+        (D.field "journal" (D.list urlEntryDecoder))
+
+
+encodeUrlState : Model -> E.Value
+encodeUrlState model =
+    E.object
+        [ ( "dice_sides", E.int model.dice_sides )
+        , ( "dice_amount", E.int model.dice_amount )
+        , ( "dice_explode", E.bool model.dice_explode )
+        , ( "table", E.string model.table )
+        , ( "icon_amount", E.int model.icon_amount )
+        , ( "journal", E.list urlEntryEncoder model.journal )
+        ]
+
+
+urlEntryEncoder : Entry -> E.Value
+urlEntryEncoder entry =
+    case entry of
+        Txt list ->
+            E.object [ ( "Txt", E.list E.string list ) ]
+
+        Img idxStr ->
+            E.object [ ( "Img", E.string idxStr ) ]
+
+
+urlEntryDecoder : D.Decoder Entry
+urlEntryDecoder =
+    D.oneOf
+        [ D.field "Txt" (D.map Txt (D.list D.string))
+        , D.field "Img" (D.map Img D.string)
+        ]
+
+
+
 -- UPDATE
 
 
@@ -200,8 +307,10 @@ type Msg
     | NewAnswer String
     | UpdateIconAmount String
     | Show
-    | NewIcon (List String)
+    | NewIcon (List Int)
     | Clear
+    | ShareUrl
+    | NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -276,18 +385,31 @@ update msg model =
             ( { model | icon_amount = String.toInt amount |> Maybe.withDefault model.icon_amount }, Cmd.none )
 
         Show ->
-            ( model, Random.generate NewIcon (randomIcons model.icons model.icon_amount) )
+            ( model, Random.generate NewIcon (randomIconIndices model.icons model.icon_amount) )
 
-        NewIcon icons ->
+        NewIcon iconIndices ->
             ( { model
                 | journal =
-                    Txt [ "Icons: " ] :: List.map (\icon -> Img icon) icons ++ model.journal
+                    Txt [ "Icons: " ] :: List.map (\idx -> Img (String.fromInt idx)) iconIndices ++ model.journal
               }
             , Cmd.none
             )
 
         Clear ->
             ( { model | journal = [] }, Cmd.batch [ clearStorage (), Cmd.none ] )
+
+        ShareUrl ->
+            let
+                stateEncoded =
+                    encodeStateToString model
+
+                shareableUrl =
+                    "?state=" ++ stateEncoded
+            in
+            ( model, Nav.pushUrl model.key shareableUrl )
+
+        NoOp ->
+            ( model, Cmd.none )
 
 
 
@@ -318,66 +440,82 @@ randomIcons icons amount =
         |> Random.map (List.map (\i -> List.Extra.getAt i icons |> Maybe.withDefault ""))
 
 
+randomIconIndices : List String -> Int -> Random.Generator (List Int)
+randomIconIndices icons amount =
+    Random.list amount (Random.int 0 (List.length icons - 1))
+
+
 
 -- VIEW
 
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
-    main_ []
-        [ header []
-            [ h1 [ class "title" ] [ text ("Dice Roller (v" ++ version ++ ")") ]
-            , Html.form []
-                [ fieldset [ class "my-inline-container" ]
-                    [ legend [] [ text "Dices" ]
-                    , div [ class "my-input" ]
-                        [ label [ for "dice_sides" ] [ text "Sides" ]
-                        , input [ id "dice_sides", type_ "text", placeholder (String.fromInt model.dice_sides), onInput UpdateDiceSides ] []
+    { title = "Dice Roller (v" ++ version ++ ")"
+    , body =
+        [ main_ []
+            [ header []
+                [ h1 [ class "title" ] [ text ("Dice Roller (v" ++ version ++ ")") ]
+                , Html.form []
+                    [ fieldset [ class "my-inline-container" ]
+                        [ legend [] [ text "Dices" ]
+                        , div [ class "my-input" ]
+                            [ label [ for "dice_sides" ] [ text "Sides" ]
+                            , input [ id "dice_sides", type_ "text", placeholder (String.fromInt model.dice_sides), onInput UpdateDiceSides ] []
+                            ]
+                        , div [ class "my-input" ]
+                            [ label [ for "dice_amount" ] [ text "Amount" ]
+                            , input [ id "dice_amount", type_ "text", placeholder (String.fromInt model.dice_amount), onInput UpdateDiceAmount ] []
+                            ]
+                        , div [ class "my-input" ]
+                            [ input [ id "dice_explode", type_ "checkbox", checked model.dice_explode, onClick UpdateDiceExplode ] []
+                            , label [ for "dice_explode" ] [ text "Explode" ]
+                            ]
+                        , button [ class "my-btn", onClick Roll ] [ text "Roll" ]
                         ]
-                    , div [ class "my-input" ]
-                        [ label [ for "dice_amount" ] [ text "Amount" ]
-                        , input [ id "dice_amount", type_ "text", placeholder (String.fromInt model.dice_amount), onInput UpdateDiceAmount ] []
+                    , fieldset [ class "my-inline-container" ]
+                        [ legend [] [ text "Tables" ]
+                        , div [ class "my-select" ]
+                            [ label [ for "tables" ] [ text "Table" ]
+                            , select [ id "tables", onInput UpdateTable ]
+                                (List.map (\x -> option [ value x, selected (x == model.table) ] [ text x ]) (Dict.keys tables))
+                            ]
+                        , button [ class "my-btn", onClick Ask ] [ text "Ask" ]
                         ]
-                    , div [ class "my-input" ]
-                        [ input [ id "dice_explode", type_ "checkbox", checked model.dice_explode, onClick UpdateDiceExplode ] []
-                        , label [ for "dice_explode" ] [ text "Explode" ]
+                    , fieldset [ class "my-inline-container" ]
+                        [ legend [] [ text "Icons" ]
+                        , div [ class "my-input" ]
+                            [ label [ for "icon_amount" ] [ text "Amount" ]
+                            , input [ id "icon_amount", type_ "text", placeholder (String.fromInt model.icon_amount), onInput UpdateIconAmount ] []
+                            ]
+                        , button [ class "my-btn", onClick Show ] [ text "Show" ]
                         ]
-                    , button [ class "my-btn", onClick Roll ] [ text "Roll" ]
+                    , button [ class "my-btn", onClick Clear ] [ text "Clear" ]
+                    , button [ class "my-btn", onClick ShareUrl ] [ text "Share" ]
                     ]
-                , fieldset [ class "my-inline-container" ]
-                    [ legend [] [ text "Tables" ]
-                    , div [ class "my-select" ]
-                        [ label [ for "tables" ] [ text "Table" ]
-                        , select [ id "tables", onInput UpdateTable ]
-                            (List.map (\x -> option [ value x, selected (x == model.table) ] [ text x ]) (Dict.keys tables))
-                        ]
-                    , button [ class "my-btn", onClick Ask ] [ text "Ask" ]
-                    ]
-                , fieldset [ class "my-inline-container" ]
-                    [ legend [] [ text "Icons" ]
-                    , div [ class "my-input" ]
-                        [ label [ for "icon_amount" ] [ text "Amount" ]
-                        , input [ id "icon_amount", type_ "text", placeholder (String.fromInt model.icon_amount), onInput UpdateIconAmount ] []
-                        ]
-                    , button [ class "my-btn", onClick Show ] [ text "Show" ]
-                    ]
-                , button [ class "my-btn", onClick Clear ] [ text "Clear" ]
+                ]
+            , article []
+                [ h1 [ class "title" ] [ text "Journal" ]
+                , div []
+                    (List.map
+                        (\entry ->
+                            case entry of
+                                Txt list ->
+                                    p [] (List.map text list)
+
+                                Img idxStr ->
+                                    let
+                                        iconSrc =
+                                            String.toInt idxStr
+                                                |> Maybe.andThen (\idx -> List.Extra.getAt idx model.icons)
+                                                |> Maybe.withDefault ""
+                                    in
+                                    svg [ SvgAttr.width "100", SvgAttr.height "100" ]
+                                        [ image [ SvgAttr.xlinkHref iconSrc, SvgAttr.width "100", SvgAttr.height "100" ] [] ]
+                        )
+                        model.journal
+                    )
                 ]
             ]
-        , article []
-            [ h1 [ class "title" ] [ text "Journal" ]
-            , div []
-                (List.map
-                    (\entry ->
-                        case entry of
-                            Txt list ->
-                                p [] (List.map text list)
-
-                            Img src ->
-                                svg [ SvgAttr.width "100", SvgAttr.height "100" ]
-                                    [ image [ SvgAttr.xlinkHref src, SvgAttr.width "100", SvgAttr.height "100" ] [] ]
-                    )
-                    model.journal
-                )
-            ]
         ]
+    }
